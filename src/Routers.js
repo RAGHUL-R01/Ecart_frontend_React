e183be7f1509b0bfce27a849e4525b9b74fb37a0
RAGHUL-R01 <raghulnishanth195@gmail.com>
2022-08-22 12:45:02 +0530

    ROutes commit

--- src/Routers.js
+++ src/Routers.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Router, Link, Routes } from "react-router-dom";
+import { BrowserRouter, Router, Link, Routes, Route } from "react-router-dom";
 import Login from "./FrontPage/login_page";
 import Register from "./FrontPage/RegistrationPage";
 import Front_page from "./FrontPage/Front_page";
@@ -10,7 +10,12 @@ const Routers = () => {
     return (
         <div>
         <BrowserRouter>
-        
+        <Routes>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/Register" element={<Register/>} />
+        <Route path="Frontpage" element={<Front_page/>} />
+        <Route path="cart" element={<Cart/>} />
+        </Routes>
         </BrowserRouter>
         </div>)
 }
